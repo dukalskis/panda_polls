a7679d796f0beb8198b52ae9ccaccece248c7635
dukalskis <dukalskis@outlook.com>
2024-05-02 09:56:36 +0300

    Style flash component

--- lib/panda_polls_web/components/core_components.ex
+++ lib/panda_polls_web/components/core_components.ex
@@ -114,9 +114,10 @@ defmodule PandaPollsWeb.CoreComponents do
       phx-click={JS.push("lv:clear-flash", value: %{key: @kind}) |> hide("##{@id}")}
       role="alert"
       class={[
-        "fixed top-2 right-2 mr-2 w-80 sm:w-96 z-50 rounded-lg p-3 ring-1",
-        @kind == :info && "bg-emerald-50 text-emerald-800 ring-emerald-500 fill-cyan-900",
-        @kind == :error && "bg-rose-50 text-rose-900 shadow-md ring-rose-500 fill-rose-900"
+        "fixed top-2 right-2 mr-2 w-80 sm:w-96 z-50 p-3",
+        "bg-white border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)]",
+        @kind == :info && "bg-emerald-50 text-emerald-800 fill-cyan-900",
+        @kind == :error && "bg-rose-50 text-rose-900 fill-rose-900"
       ]}
       {@rest}
     >
@@ -127,7 +128,7 @@ defmodule PandaPollsWeb.CoreComponents do
       </p>
       <p class="mt-2 text-sm leading-5"><%= msg %></p>
       <button type="button" class="group absolute top-1 right-1 p-2" aria-label="close">
-        <.icon name="hero-x-mark-solid" class="h-5 w-5 opacity-40 group-hover:opacity-70" />
+        <.icon name="hero-x-mark-solid" class="h-5 w-5 text-black group-hover:text-brand" />
       </button>
     </div>
     """
